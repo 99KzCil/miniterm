19625de8aaf7b685579a4c14630d689ba73998ff
Enes <enesaltinkaya@gmail.com>
2018-11-29 01:38:18 +0300

    more

---
+++ src/main/java/com/alessoft/Config/WebApplicationConfig.java
@@ -0,0 +1,29 @@
+package com.alessoft.Config;
+
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebApplicationConfig implements WebMvcConfigurer {
+
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        System.err.println(registry.toString());
+        registry.addViewController("/notFound").setStatusCode(HttpStatus.OK).setViewName("redirect:/miniterm/");
+    }
+
+    @Bean
+    public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> containerCustomizer() {
+        return container -> {
+            container.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/notFound"));
+        };
+    }
+
+}
